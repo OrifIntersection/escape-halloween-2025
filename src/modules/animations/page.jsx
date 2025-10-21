5cb97c3fd2b0cbf48d7c961b426ce01ae0864d8e
Eve_rest2 <yaelfavresectioninformatique@gmail.com>
2025-10-21 16:42:18 +0200

    Aperçu fini

--- src/modules/animations/page.jsx
+++ src/modules/animations/page.jsx
@@ -2,153 +2,127 @@ import { useState, useEffect } from "react";
 import image from '../../assets/images/sequences/image.png';
 import "./style.css";
 
-function SequenceItem({data, i, ActiveSequenceFunction, setActiveSequence, setActivePreview}) {
+function SequenceItem({ sequence, setActiveSequence }) {
 
   // Fonction du click sur la séquence
 
   function handleClick(e) {
     e.preventDefault();
-    setActiveSequence(ActiveSequenceFunction);
+    setActiveSequence(sequence);
   }
 
-
-
-  return (<>{data ? <>
+  return (<>
     <div className="AnimationSequenceContainer" onClick={handleClick}>
       <div>
-        <img alt="aperçu de séquence" src={image} style={{height:"100px",width:"100px"}}/>
+        <img alt="aperçu de séquence" src={image} style={{ height: "100%", width: "150px" }} />
       </div>
 
       <div className="AnimationSequenceInfoCont">
         <div className="AnimationSequenceInfoTitle">
-          {data[i]?.title}
+          {sequence.title}
         </div>
         <div className="AnimationSequenceInfoDescr">
-          {data[i]?.description}
+          {sequence.description}
         </div>
       </div>
 
       <div>
-        {data[i]?.order}
+        {sequence.order}
       </div>
 
-      <div style={{paddingLeft:"2%"}}>
-        {data[i]?.duration}
+      <div style={{ paddingLeft: "2%" }}>
+        {sequence.duration}
       </div>
-    </div>  </>:<>Loading</>
-  }  </>)  
+    </div>
+    <hr style={{color:"black"}}/>
+  </>)
+}
+
+
+/*
+
+   { 
+            (Data) ? <video controls="true" src={Data[0].file} />  : <> </>    
+          }
+ */
+
+function ActiveSequencePreview({ sequence }) {
+ 
+  return (<>
+    <div style={{overflow:"hidden"}} className="AnimationSequencePreviewCont">
+      <video style={{objectFit:"cover", width:"100%", height:"100%"}} controls="true" src={sequence.file} type="video/mp4" autoPlay muted playsInline/> 
+    </div>
+  </>);
+}
+
+function NoActiveSequencePreview() {
+  return (
+    <div style={{margin:"20px"}} className="AnimationSequencePreviewCont">
+      <p className="AnimationSequenceP">
+        Désactivé
+      </p>
+    </div>
+  );
 }
 
 function AnimationsPage() {
-  
-  let i = 0;
-//------------------------------------------------------------------------------------------
-//                                        Séquences
-//------------------------------------------------------------------------------------------
 
-// State de la vidéo de la séquence active
-  const [ActivePreview, setActivePreview] = useState(null);
+  //------------------------------------------------------------------------------------------
+  //                                        Séquences
+  //------------------------------------------------------------------------------------------ 
 
-// Affichage de l'aperçu de séquence activée, désactivée 
+  // State de la séquence active + son aperçu
+  const [activeSequence, setActiveSequence] = useState(null);
 
-  function ActiveSequenceFunction() {
-    return(<>
-      <div className="AnimationSequencePreviewCont"> 
-        <p>Active</p>
-        {}
-      </div>
-    </>);
-  }
+  //------------------------------------------------------------------------------------------
+  //                                         Données 
+  //------------------------------------------------------------------------------------------
+  const [sequences, setSequences] = useState(null);
 
-  function DesactiveSequenceFunction() {
-    return(            
-      <div className="AnimationSequencePreviewCont"> 
-        <p>
-          Désactivé
-        </p>
-      </div>
-    );
-  }
-
-// State de la séquence active + son aperçu
-
-  const [ActiveSequence, setActiveSequence] = useState(DesactiveSequenceFunction);
-
-//------------------------------------------------------------------------------------------
-//                                      Données 
-//------------------------------------------------------------------------------------------
-
-  const [Data, setData] = useState();
-
-/*                                                  /\
-   Récupération des données via useState Sequences /||\
-                                                    ||
-                                                    ||
-*/
-
+  /*                                                  /\
+     Récupération des données via useState Sequences /||\
+                                                      ||
+                                                      ||
+  */
   useEffect(() => {
     fetch("/mocks/sequence.json")
-        .then(response => {
-          if (!response.ok) {
-            throw new Error("Error");
-          }
-          return response.json();
-        })
-        .then(data => {
-          console.log("Fetch succes, update: ", data);
-          setData(data)
-        })
-        .catch(error => {
-          console.log("Catch Error : ", error);
-        })
-    }, []);
-
-//------------------------------------------------------------------------------------------
-
-  useEffect(()=> {
-   // setTimeout(() => {
-
-      console.log("useEffect")
-        if (Data) {
-          console.log("Data is useEffect")
-          const parent = document.getElementsByClassName('AnimationSequencePreviewCont')
-          let video = document.createElement('video');
-          video.src = Data[0].file;
-          video.controls = true;
-          video.setAttribute('controls', true)
-          video.width = 640;
-          setActivePreview(video)
-
-          parent[1].appendChild(video)        
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Error");
         }
-     // }, 3000);
-     }, [ActiveSequence])
+        return response.json();
+      })
+      .then(data => {
+        console.log("Fetch succes, update: ", data);
+        setSequences(data)
+      })
+      .catch(error => {
+        console.log("Catch Error : ", error);
+      })
+  }, []);
 
-  useEffect(()=> {
-
-      console.log(ActivePreview, "ActivePreview")
-  }, [ActivePreview])
+  //------------------------------------------------------------------------------------------
 
   return (<>
-      <div className="AnimationContainer">
-            <div className="AnimationSequencePreviewCont">
-              {ActiveSequence}
-            </div>
-            <div>
-              
-            </div>
-            <div className="AnimationSequenceTitles">
-              <p>Séquences :</p>
-              <p>Position</p>
-              <p>Durée</p>
-              </div>
-            <div className="AnimationSequencesListCont">
-
-            <SequenceItem i={0} data={Data} ActiveSequenceFunction={ActiveSequenceFunction} setActiveSequence={setActiveSequence} setActivePreview={setActivePreview}/>
-            <SequenceItem i={1} data={Data} ActiveSequenceFunction={ActiveSequenceFunction} setActiveSequence={setActiveSequence} setActivePreview={setActivePreview}/>
-            <SequenceItem i={2} data={Data} ActiveSequenceFunction={ActiveSequenceFunction} setActiveSequence={setActiveSequence} setActivePreview={setActivePreview}/>
-            </div>
+    <div className="AnimationContainer">
+      <div className="AnimationTable">
+        <p style={{display:"flex", flexDirection:"row"}}>Écran principal</p>
+        <p>Écran secondaire</p>
+        <p>Écran input</p>
+        <p>S</p>
       </div>
+      {(activeSequence) ? <ActiveSequencePreview sequence={activeSequence} /> : <NoActiveSequencePreview />}
+      <div className="AnimationSequenceTitles">
+        <p>Séquences :</p>
+        <p>Position</p>
+        <p>Durée</p>
+      </div>
+      <div className="AnimationSequencesListCont">
+        {(sequences) ? <>
+          {sequences.map((sequenceItem) => <SequenceItem sequence={sequenceItem} setActiveSequence={setActiveSequence} />)}
+        </> : <>Loading sequences</>}
+      </div>
+    </div>
   </>)
 }
 
