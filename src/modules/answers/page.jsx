bdb17fe07b86482585c91c1380399e5fd1c5ed1f
rschmutz-orif <raphael.schmutz@orif.ch>
2025-10-23 07:15:52 +0200

    Code review - Base Structure sample

--- src/modules/answers/page.jsx
+++
@@ -1,7 +0,0 @@
-function AnswersPage() {
-  return <>
-    <h1>Une page permettant aux participants de rentrer les réponses aux enigmes pour progresser dans le jeu.</h1>
-  </>
-}
-
-export default AnswersPage
--- src/modules/clues/page.jsx
+++
@@ -1,7 +0,0 @@
-function CluesPage() {
-  return <>
-    <h1>Une page permettant d'afficher aux participants des indices si ils progressent trop lentement.</h1>
-  </>
-}
-
-export default CluesPage
